804fec26ae0338485df1dc770adc9c35defc3d05
John Smith <example@stanford.edu>
2013-07-13 03:17:06 +0000

    wow

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express.createServer(express.logger());
-var m= fs.readFileSync(index.html)
-var lol=buffer.toString("utf-8", 0, 12)
+//var m= fs.readFileSync(index.html)
+//var lol=buffer.toString("utf-8", 0, 12)
 
 app.get('/', function(request, response) {
-  response.send('wtf');
+  response.send("wtf");
 });
 
 var port = process.env.PORT || 5000;
