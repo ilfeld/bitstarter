a0564b97c59da6a39d3e139309df1c878e9dc4e7
John Smith <example@stanford.edu>
2013-07-13 14:10:37 +0000

    commented out fsread

--- web.js
+++ web.js
@@ -2,11 +2,11 @@ buf = new Buffer(256);
 var fs = require('fs');
 var express = require('express');
 var app = express.createServer(express.logger());
-fs.readFile('index.html', function (err, data) {
+/*fs.readFile('index.html', function (err, data) {
   if (err) throw err;
   console.log(data);
   lol=buffer.toString("utf-8");
-});
+});*/
 mes="please"
 
 app.get('/', function(request, response) {
