c186f7a37712ffe1098cdd9d39156e44b0dc4f48
John Smith <example@stanford.edu>
2013-07-13 03:10:53 +0000

    changed txt

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var m= fs.readFileSync(index.html)
+var lol=buffer.toString("utf-8", 0, 12)
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(lol);
 });
 
 var port = process.env.PORT || 5000;
